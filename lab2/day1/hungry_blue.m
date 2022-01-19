308711a8dd06ff68e954ca84386850da21bd6dc1
Kristina Rogacheva <33569375+kristina-rogacheva@users.noreply.github.com>
2022-01-19 13:23:42 +0100

    IMG MANIPULATION: func in different files
    changed the luminance func - prints the histograms before the manipulation and after

---
+++ lab2/day1/hungry_blue.m
@@ -0,0 +1,21 @@
+% Blue - most costly component : function to reduce its value
+% takes as input the image to manipulate and the desired reduction percentage 
+function new_img = hungry_blue(img, reduction_percentage)
+
+    if reduction_percentage < 0 || reduction_percentage > 100
+        error("ERROR: the percentage should be between 0 and 100!!! \n"); 
+    end
+
+    % picture dimension
+    height = length(img(:, 1, 1));
+    width  = length(img(1, :, 1));
+
+    new_img = img;
+    
+    % newblue = oldvalue - oldvalue*percentage/100;
+    for i = 1 : height
+        for j = 1 : width
+            new_img(i,j,3) = (double(new_img(i,j,3)) - ((double(new_img(i,j,3)) * reduction_percentage)/100));
+        end
+    end
+end
