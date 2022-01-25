67a418c7867e8228bc169be96e7863680dd53a26
Giuseppe Carrubba <s269182@studenti.polito.it>
2022-01-25 16:47:06 +0100

    modified and commented day1.m ready to analyze multiple images

--- lab2/day1.m
+++ lab2/day1.m
@@ -6,32 +6,44 @@ close all
 %image_list_2.dir = dir(fullfile('./BSR/BSDS500/data/images/train/', '*.jpg'));
 %image_list_3.dir = dir(fullfile('./screenshots/', '*.JPG'));
 
-image = imread('screenshots/screenshot4.png');
-[H, W, channels] = size(image); %function of imaging processing packet
+%creating data structure for analyze the images
 
+
+%
+%stratup data setting
+%for index = 1:length(image_list_1.dir)
+    
+%end
+
+image = imread('screenshots/screenshot4.png');
+%[H, W, channels] = size(image); %function of imaging processing packet
+
+%analyze original image
 power_original = power_consumption(image);
 RGB_distribution(image);
 
+%apply transformation
 image_hungry_blue = hungry_blue(image,20);
 image_histo_eq = histogram_eq(image);
+%here the 3rd transformation
 
+%analyze power consumption of the new images
 power_hungry_blue = power_consumption(image_hungry_blue);
 power_histo_eq = power_consumption(image_histo_eq);
 
+%compute the difference
 difference1_blue = image_difference(image, image_hungry_blue);
 difference1_histo = image_difference(image, image_histo_eq);
+%compute the distortion in percentage
+distortion_blue = distortion(difference1_blue, image);
 
-
-
-distortion_blue = distortion(difference1_blue, H, W);
-%distortion_histo = distortion2(image, image_histo_eq);
-
+%show new images after transformation
 figure;
 imshow(image);
 figure;
 imshow(image_hungry_blue);
 figure;
-imshow(image_histo_eq);
+imshow(image_histo_eq); %this will print image and also histogram of gray equalization
 
 
 
--- lab2/distortion.m
+++ lab2/distortion.m
@@ -1,4 +1,6 @@
-function distortion = distortion(difference, W, H)
+function distortion = distortion(difference,image)
+    
+    [H, W, channels] = size(image);
     
     distortion = (difference / (W * H * sqrt(100.^2 + 255.^2 + 255.^2))) * 100;
 
