3ebae06bb926b614583629ffbd268a66d6fef9fc
Kristina Rogacheva <33569375+kristina-rogacheva@users.noreply.github.com>
2022-01-27 13:24:42 +0100

    IMG MANIPULATION: changed the custom function

--- lab2/custom.m
+++ lab2/custom.m
@@ -5,31 +5,53 @@ function new_img = custom(img, reduction_percentage)
         error("ERROR: the percentage should be between 0 and 100!!! \n"); 
     end
     
-    %figure;
-    %imhist(img);
-    %imshow(img);
+    figure;
+    imhist(img);
+    imshow(img);
 
      % picture dimension
     height = length(img(:, 1, 1));
     width  = length(img(1, :, 1));
 
     new_blue = img;
-    new_img = histeq(new_blue); %modifica necessaria
-
-    %figure;
-    %imhist(new_img);
-    %imshow(new_img);
     
     % newblue = oldvalue - oldvalue*percentage/100;
     for i = 1 : height
         for j = 1 : width
-            new_img(i,j,3) = (double(new_img(i,j,3)) - ((double(new_img(i,j,3)) * reduction_percentage)/100));
+            new_blue(i,j,3) = (double(new_blue(i,j,3)) - ((double(new_blue(i,j,3)) * reduction_percentage)/100));
         end
     end
 
-    %figure;
-    %imhist(new_img);
-    %imshow(new_img);
+    figure;
+    imhist(new_blue);
+    imshow(new_blue);
 
+    new_green = new_blue;
+    
+    % new_green = oldvalue - oldvalue*percentage/100;
+    for i = 1 : height
+        for j = 1 : width
+            new_green(i,j,2) = (double(new_green(i,j,2)) - ((double(new_green(i,j,2)) * reduction_percentage)/100));
+        end
+    end
 
+    figure;
+    imhist(new_green);
+    imshow(new_green);
+
+    new_red = new_green;
+    
+    % new_red = oldvalue - oldvalue*percentage/100;
+    for i = 1 : height
+        for j = 1 : width
+            new_red(i,j,1) = (double(new_red(i,j,1)) - ((double(new_red(i,j,1)) * reduction_percentage)/100));
+        end
+    end
+
+    figure;
+    imhist(new_red);
+    imshow(new_red);
+
+    
+    new_img = new_red;
 end
