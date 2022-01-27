8bf44c78ceba6c191af48793e3dac5b2a4e5b950
Giuseppe Carrubba <s269182@studenti.polito.it>
2022-01-27 13:19:10 +0100

    now we create data structure for image lists and day1 launches function that fills data structure

---
+++ lab2/auto_image_list1.m
@@ -0,0 +1,206 @@
+function structure1 = auto_image_list1(images_list1)
+
+%arrays for list1
+images_list1.name = strings(1, length(images_list1)); %create array for names
+images_list1.original_power = zeros(1, length(images_list1));
+%different tries for different constraints of distortion
+%value 1
+images_list1.power_hungry_blue1 = zeros(1, length(images_list1));
+images_list1.power_histo_eq = zeros(1, length(images_list1));
+images_list1.power_custom1 = zeros(1, length(images_list1));
+
+images_list1.differnce_hungry_blue1 = zeros(1, length(images_list1));
+images_list1.differnce_histo_eq = zeros(1, length(images_list1));
+images_list1.differnce_custom1 = zeros(1, length(images_list1));
+
+images_list1.distortion_hungry_blue1 = zeros(1, length(images_list1));
+images_list1.distortion_histo_eq = zeros(1, length(images_list1));
+images_list1.distortion_custom1 = zeros(1, length(images_list1));
+
+%value 5
+images_list1.power_hungry_blue5 = zeros(1, length(images_list1));
+images_list1.power_custom5 = zeros(1, length(images_list1));
+
+images_list1.differnce_hungry_blue5 = zeros(1, length(images_list1));
+images_list1.differnce_custom5 = zeros(1, length(images_list1));
+
+images_list1.distortion_hungry_blue5 = zeros(1, length(images_list1));
+images_list1.distortion_custom5 = zeros(1, length(images_list1));
+%value 10
+images_list1.power_hungry_blue10 = zeros(1, length(images_list1));
+images_list1.power_custom10 = zeros(1, length(images_list1));
+
+images_list1.differnce_hungry_blue10 = zeros(1, length(images_list1));
+images_list1.differnce_custom10 = zeros(1, length(images_list1));
+
+images_list1.distortion_hungry_blue10 = zeros(1, length(images_list1));
+images_list1.distortion_custom10 = zeros(1, length(images_list1));
+
+%average
+images_list1.average_power_hungry_blue1 = 0;
+images_list1.average_power_histo_eq = 0;
+images_list1.average_power_custom1 = 0;
+
+images_list1.average_distortion_hungry_blue1 = 0;
+images_list1.average_distortion_histo_eq = 0;
+images_list1.average_distortion_custom1 = 0;
+
+images_list1.average_power_hungry_blue5 = 0;
+images_list1.average_power_custom5 = 0;
+
+images_list1.average_distortion_hungry_blue5 = 0;
+images_list1.average_distortion_custom5 = 0;
+
+images_list1.average_power_hungry_blue10 = 0;
+images_list1.average_power_custom10 = 0;
+
+images_list1.average_distortion_hungry_blue10 = 0;
+images_list1.average_distortion_custom10 = 0;
+
+%loop for each image in the folder
+for k = 1 : length(images_list1.dir)
+
+    image_path = strcat(images_list1.dir(k).folder, '/', images_list1.dir(k).name);
+    images_list1.name(k) = images_list1.dir(k).name;
+    image = imread(image_path);
+    [H, W, channels] = size(image); %function of imaging processing packet
+
+    if channels == 1 %in case the image is in black and white without colors
+        image = gray2rgb(image);
+        %gray_distribution(image);
+    end
+
+    images_list1.original_power(k) = power_consumption(image);
+
+    %apply transformations with 1%
+    image_hungry_blue = hungry_blue(image,1);
+    image_histo_eq = histogram_eq(image);
+    image_custom = custom(image, 1);
+
+    %evaluate power after transformations
+    images_list1.power_hungry_blue1(k) = power_consumption(image_hungry_blue);
+    images_list1.power_histo_eq(k) = power_consumption(image_histo_eq);
+    images_list1.power_custom1(k) = power_consumption(image_custom);
+
+    %compute difference
+    images_list1.differnce_hungry_blue1(k) = image_difference(image, image_hungry_blue);
+    images_list1.differnce_histo_eq(k) = image_difference(image, image_histo_eq);
+    images_list1.differnce_custom1(k) = image_difference(image, image_custom);
+
+    %compute distortion
+    images_list1.distortion_hungry_blue1(k) = distortion(images_list1.differnce_hungry_blue1(k), image);
+    images_list1.distortion_histo_eq(k) = distortion(images_list1.differnce_histo_eq(k), image);
+    images_list1.distortion_custom1(k) = distortion(images_list1.differnce_custom1(k), image);  
+
+    %apply transformation with 5%
+    image_hungry_blue = hungry_blue(image,5);
+    image_custom = custom(image, 5);
+
+    %evaluate power after transformations
+    images_list1.power_hungry_blue5(k) = power_consumption(image_hungry_blue);
+    images_list1.power_custom5(k) = power_consumption(image_custom);
+
+    %compute difference
+    images_list1.differnce_hungry_blue5(k) = image_difference(image, image_hungry_blue);
+    images_list1.differnce_custom5(k) = image_difference(image, image_custom);
+
+    %compute distortion
+    images_list1.distortion_hungry_blue5(k) = distortion(images_list1.differnce_hungry_blue5(k), image);
+    images_list1.distortion_custom5(k) = distortion(images_list1.differnce_custom5(k), image);  
+
+    %apply transformation with 10%
+    image_hungry_blue = hungry_blue(image,10);
+    image_custom = custom(image, 10);
+
+    %evaluate power after transformations
+    images_list1.power_hungry_blue10(k) = power_consumption(image_hungry_blue);
+    images_list1.power_custom10(k) = power_consumption(image_custom);
+
+    %compute difference
+    images_list1.differnce_hungry_blue10(k) = image_difference(image, image_hungry_blue);
+    images_list1.differnce_custom10(k) = image_difference(image, image_custom);
+
+    %compute distortion
+    images_list1.distortion_hungry_blue10(k) = distortion(images_list1.differnce_hungry_blue10(k), image);
+    images_list1.distortion_custom10(k) = distortion(images_list1.differnce_custom10(k), image);
+
+    %avg power consumption
+    %1
+    images_list1.average_power_hungry_blue1 = images_list1.average_power_hungry_blue1 + (images_list1.original_power(k) - images_list1.power_hungry_blue1(k));
+    images_list1.average_power_histo_eq =  images_list1.average_power_histo_eq + (images_list1.original_power(k) - images_list1.power_histo_eq(k));
+    images_list1.average_power_custom1 = images_list1.average_power_custom1 + (images_list1.original_power(k) - images_list1.power_custom1(k));
+    %5
+    images_list1.average_power_hungry_blue5 = images_list1.average_power_hungry_blue5 + (images_list1.original_power(k) - images_list1.power_hungry_blue5(k));
+    images_list1.average_power_custom5 = images_list1.average_power_custom5 + (images_list1.original_power(k) - images_list1.power_custom5(k));
+    %10
+    images_list1.average_power_hungry_blue10 = images_list1.average_power_hungry_blue10 + (images_list1.original_power(k) - images_list1.power_hungry_blue10(k));
+    images_list1.average_power_custom10 = images_list1.average_power_custom10 + (images_list1.original_power(k) - images_list1.power_custom10(k));
+
+    %avg distortion
+    %1
+    images_list1.average_distortion_hungry_blue1 = images_list1.average_distortion_hungry_blue1 + images_list1.distortion_hungry_blue1(k);
+    images_list1.average_distortion_histo_eq = images_list1.average_distortion_histo_eq + images_list1.distortion_histo_eq(k);
+    images_list1.average_distortion_custom1 = images_list1.average_distortion_custom1 + images_list1.distortion_custom1(k);
+    %5
+    images_list1.average_distortion_hungry_blue5 = images_list1.average_distortion_hungry_blue5 + images_list1.distortion_hungry_blue5(k);
+    images_list1.average_distortion_custom5 = images_list1.average_distortion_custom5 + images_list1.distortion_custom5(k);
+    %10
+    images_list1.average_distortion_hungry_blue10 = images_list1.average_distortion_hungry_blue10 + images_list1.distortion_hungry_blue10(k);
+    images_list1.average_distortion_custom10 = images_list1.average_distortion_custom5 + images_list1.distortion_custom10(k);
+
+
+end 
+
+%1 power average
+images_list1.average_power_hungry_blue1 = images_list1.average_power_hungry_blue1/k;
+images_list1.average_power_histo_eq = images_list1.average_power_histo_eq/k;
+images_list1.average_power_custom1 = images_list1.average_power_custom1/k;
+%5
+images_list1.average_power_hungry_blue5 = images_list1.average_power_hungry_blue5/k;
+images_list1.average_power_custom5 = images_list1.average_power_custom5/k;
+%10
+images_list1.average_power_hungry_blue10 = images_list1.average_power_hungry_blue10/k;
+images_list1.average_power_custom10 = images_list1.average_power_custom10/k;
+%distortion average
+%1
+images_list1.average_distortion_hungry_blue1 = images_list1.average_distortion_hungry_blue1/k;
+images_list1.average_distortion_histo_eq = images_list1.average_distortion_histo_eq/k;
+images_list1.average_distortion_custom1 = images_list1.average_distortion_custom1/k;
+%5
+images_list1.average_distortion_hungry_blue5 = images_list1.average_distortion_hungry_blue5/k;
+images_list1.average_distortion_custom5 = images_list1.average_distortion_custom5/k;
+%10
+images_list1.average_distortion_hungry_blue10 = images_list1.average_distortion_hungry_blue10/k;
+images_list1.average_distortion_custom10 = images_list1.average_distortion_custom10/k;
+
+
+x_savings_hungry_blue = [images_list1.average_power_hungry_blue1, images_list1.average_power_hungry_blue5, images_list1.average_power_hungry_blue10];
+y_distortion_hungry_blue = [images_list1.average_distortion_hungry_blue1, images_list1.average_distortion_hungry_blue5, images_list1.average_distortion_hungry_blue10];
+
+x_savings_histo_eq = images_list1.average_power_histo_eq;
+y_distortion_histo_eq = images_list1.average_distortion_histo_eq;
+
+x_savings_custom = [images_list1.average_power_custom1,images_list1.average_power_custom5,images_list1.average_power_custom10];
+y_distortion_custom = [images_list1.average_distortion_custom1 ,images_list1.average_distortion_custom5,images_list1.average_distortion_custom10];
+
+figure;
+plot(y_distortion_hungry_blue,x_savings_hungry_blue, '-o');
+hold on;
+plot(y_distortion_histo_eq, x_savings_histo_eq, '-o');
+hold on;
+plot(y_distortion_custom, x_savings_custom, '-o');
+xlabel('AVG DISTORTION [%]');
+ylabel('AVG POWER CONSUMPTION');
+legend('Hungry blue', 'Histogram equalization', 'Custom');
+
+
+structure1 = images_list1;
+
+end
+
+
+
+
+
+
+
--- lab2/day1.m
+++ lab2/day1.m
@@ -1,62 +1,18 @@
+clear
+clc
 close all
 
+images_list1 = struct;
+images_list2 = struct;
+images_list3 = struct;
+%create data structure one for each folder
+images_list1.dir= dir(fullfile("misc/*.tiff")); %39 images
+images_list2.dir = dir(fullfile("BSR_bsds500/BSR/BSDS500/data/images/train/*.jpg")); %200 images
+images_list3.dir = dir(fullfile("screenshots/*.png")); % 5 images 
 
-%magari qua proviamo a caricare tutto tipo struttura dati
-%image_list_1.dir = dir(fullfile('./misc/', '*.tiff'));
-%image_list_2.dir = dir(fullfile('./BSR/BSDS500/data/images/train/', '*.jpg'));
-%image_list_3.dir = dir(fullfile('./screenshots/', '*.JPG'));
+%launch scripts that will fill data structure
+images_list1 = auto_image_list1(images_list1);
+%images_list2 = auto_image_list2(images_list2); % se testo la lista 2 mi dá
+%problemi nell'image difference non so perché
 
-%creating data structure for analyze the images
-
-
-%
-%stratup data setting
-%for index = 1:length(image_list_1.dir)
-    
-%end
-
-%image = imread('screenshots/screenshot1.png');
-image = imread('misc\7.1.10.tiff');
-[H, W, channels] = size(image); %function of imaging processing packet
-
-if channels == 1
-    image = gray2rgb(image);
-    gray_distribution(image);
-end
-
-%analyze original image
-power_original = power_consumption(image);
-%RGB_distribution(image);
-
-%apply transformation
-image_hungry_blue = hungry_blue(image,10); 
-image_histo_eq = histogram_eq(image);
-image_custom = custom(image, 20); %here the 3rd custom transformation
-
-%analyze power consumption of the new images
-power_hungry_blue = power_consumption(image_hungry_blue);
-power_histo_eq = power_consumption(image_histo_eq);
-power_custom = power_consumption(image_custom);
-
-
-%compute the difference
-difference1_blue = image_difference(image, image_hungry_blue);
-difference1_histo = image_difference(image, image_histo_eq);
-difference1_custom = image_difference(image, image_custom);
-
-%compute the distortion in percentage
-distortion_blue = distortion(difference1_blue, image);
-distortion_histogram = distortion(difference1_histo, image);
-distortion_custom = distortion(difference1_custom, image);
-
-
-%show new images after transformation
-figure;
-imshow(image);
-figure;
-imshow(image_hungry_blue);
-figure;
-imshow(image_histo_eq); %this will print image and also histogram of gray equalization
-
-
-
+images_list3 = auto_image_list3(images_list3);
