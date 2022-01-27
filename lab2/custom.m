06862635acd37efa6c480e2073ee0ca0403e2ca4
Giuseppe Carrubba <s269182@studenti.polito.it>
2022-01-27 11:50:27 +0100

    adjusted automatized script for list misc

--- lab2/custom.m
+++ lab2/custom.m
@@ -5,20 +5,20 @@ function new_img = custom(img, reduction_percentage)
         error("ERROR: the percentage should be between 0 and 100!!! \n"); 
     end
     
-    figure;
-    imhist(img);
-    imshow(img);
+    %figure;
+    %imhist(img);
+    %imshow(img);
 
      % picture dimension
     height = length(img(:, 1, 1));
     width  = length(img(1, :, 1));
 
     new_blue = img;
-    new_img = histeq(new_blue);
+    new_img = histeq(new_blue); %modifica necessaria
 
-    figure;
-    imhist(new_img);
-    imshow(new_img);
+    %figure;
+    %imhist(new_img);
+    %imshow(new_img);
     
     % newblue = oldvalue - oldvalue*percentage/100;
     for i = 1 : height
@@ -27,9 +27,9 @@ function new_img = custom(img, reduction_percentage)
         end
     end
 
-    figure;
-    imhist(new_img);
-    imshow(new_img);
+    %figure;
+    %imhist(new_img);
+    %imshow(new_img);
 
 
 end
--- lab2/histogram_eq.m
+++ lab2/histogram_eq.m
@@ -1,11 +1,11 @@
 %% luminance reduction 
 function hist_eq_img = histogram_eq(img)
 
-    figure;
-    imhist(img);
+    %figure;
+    %imhist(img);
     
     hist_eq_img = histeq(img);
 
-    figure;
-    imhist(hist_eq_img);
+    %figure;
+    %imhist(hist_eq_img);
 end
