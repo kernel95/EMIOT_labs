e6adde41e3e9f6b62aa8a1166acabc51266ad9c6
Giuseppe Carrubba <s269182@studenti.polito.it>
2022-01-31 10:31:46 +0100

    now we plot also the original photo

--- lab2/show_photos.m
+++ lab2/show_photos.m
@@ -3,6 +3,10 @@ close all
 image = imread("misc\4.2.05.tiff");
 [H, W, channels] = size(image);
 
+figure0 = figure;
+imshow(image);
+saveas(figure0, 'figures/Original_photo.jpg');
+
 image_hungry_blue = hungry_blue(image,5);
 image_histo_eq = histogram_eq(image);
 image_custom = custom(image, 5);
